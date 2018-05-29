6602a6b9e990871d47dd32aac5c2e0498f5b29bb
Tanvir Islam Streame <streametanvir@gmail.com>
2018-05-30 00:24:33 +0600

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,7 +2,10 @@ import bs4 as bs
 import re
 import urllib.request
 
-sauce=urllib.request.urlopen("http://en.prothomalo.com/bangladesh/news/176492/Take-leave-join-Hasina%E2%80%99s-electioneering-in-next")
+
+urlText=input("Insert URl : ")
+searchText=input("keyword :")
+sauce=urllib.request.urlopen(urlText)
 soup=bs.BeautifulSoup(sauce,'lxml')
 
 
@@ -18,6 +21,20 @@ soup=bs.BeautifulSoup(sauce,'lxml')
 
 
 #print(soup.find_all('p')[1].text)
-for par in soup.find_all('p'):
-    if re.search('(?i)platform ', par.text):
+   
+print("From h1")
+for par in soup.body.find_all('h1'):
+    if searchText.lower() in par.text.lower() :
         print(par.text)
+
+print("From a")
+for par in soup.body.find_all('a'):
+    if searchText.lower() in par.text.lower() :
+        print(par.text)
+        
+print("From p")
+for par in soup.body.find_all('p'):
+    if searchText.lower() in par.text.lower() :
+        print(par.text)   
+
+    
